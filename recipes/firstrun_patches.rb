037848fbccaed81b2dcc4a94fda141324ee4dc78
Corey Hemminger <hemminger@hotmail.com>
2020-11-25 14:41:14 -0600

    fix linux script bugs and added windows regex filter of updates
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

--- recipes/firstrun_patches.rb
+++ recipes/firstrun_patches.rb
@@ -32,42 +32,172 @@ when 'linux'
   )
   execute 'linux-upgrade-once' do
     command cmd
-    action :nothing
     notifies :request_reboot, 'reboot[firstrun_patches]', :delayed if node['autopatch_ii']['auto_reboot_enabled']
+    notifies :create_if_missing, "file[#{Chef::Config[:file_cache_path]}/autopatch.txt]", :immediately
+    not_if { ::File.exist?("#{Chef::Config[:file_cache_path]}/autopatch.txt") }
   end
+
 when 'windows'
   powershell_script 'win-update' do
     code <<-EOH
-    Function WSUSUpdate {
-      $Criteria = "IsInstalled=0 and Type='Software'"
-      $Searcher = New-Object -ComObject Microsoft.Update.Searcher
-      try {
-        $SearchResult = $Searcher.Search($Criteria).Updates
-        if ($SearchResult.Count -eq 0) {
-          Write-Output "There are no applicable updates."
-          exit
-        }
-        else {
-          $Session = New-Object -ComObject Microsoft.Update.Session
-          $Downloader = $Session.CreateUpdateDownloader()
-          $Downloader.Updates = $SearchResult
-          $Downloader.Download()
-          $Installer = New-Object -ComObject Microsoft.Update.Installer
-          $Installer.Updates = $SearchResult
-          $Result = $Installer.Install()
+    $reg = "#{node['autopatch_ii']['updates_to_skip']}"
+    # Main Logic
+    try
+    {
+      Write-Output "Starting script..."
+      $UpdatesToDownload = New-Object -ComObject "Microsoft.Update.UpdateColl"
+      $objServiceManager = New-Object -ComObject "Microsoft.Update.ServiceManager" #Support local instance only
+      $objSession = New-Object -ComObject "Microsoft.Update.Session" #Support local instance only
+      $objSearcher = $objSession.CreateUpdateSearcher()
+      $search = "IsInstalled = 0"
+      Write-Output "Searching for new updates..."
+      try
+      {
+        $objResults = New-Object -ComObject "Microsoft.Update.UpdateColl"
+        $notInstalled = $objSearcher.Search($search)
+        foreach ($temp in $notInstalled.Updates)
+        {
+          if ($reg -eq "")
+          {
+            $objResults.Add($temp) | Out-Null
+          }
+          else
+          {
+            if ($temp.Title -notmatch $reg)
+            {
+              $objResults.Add($temp) | Out-Null
+            }
+          }
         }
       }
-      catch {
-        Write-Output "There are no applicable updates."
+      catch
+      {
+        throw "An error occurred while search for updates to be installed."
+      } #End Catch
+
+      if ($objResults.Count -eq 0)
+      {
+        Write-Output "Found $($notInstalled.Updates.Count) new updates."
+        Write-Output "Found $($objResults.Count) after filter applied"
+        Write-Output "No selected updates found to be installed."
+        Exit 0
+      }
+      else
+      {
+        # There are updates to be installed.
+        Write-Output "Found $($notInstalled.Updates.Count) new updates."
+        Write-Output "Found $($objResults.Count) after filter applied."
+        foreach ($update in $objResults)
+        {
+          Write-Output "Update to install '$($update.Title)'"
+        }
+      }
+      # === Download Updates ===
+      Write-Output "Starting download of updates..."
+      $updDownloader = $objSession.CreateUpdateDownloader()
+      $DownloadedUpdateCollection = New-Object -ComObject "Microsoft.Update.UpdateColl"
+      foreach ($update in $objResults)
+      {
+        $objCollectionTmp = New-Object -ComObject "Microsoft.Update.UpdateColl"
+        $objCollectionTmp.Add($Update) | Out-Null
+        $Downloader = $objSession.CreateUpdateDownloader()
+        $Downloader.Updates = $objCollectionTmp
+        $DownloadResult = $Downloader.Download()
+        if(($DownloadResult.ResultCode -eq 2) -and ($($update.EulaAccepted) -eq $true))
+        {
+          Write-Output "$($update.Title) was successfully downloaded"
+          $DownloadedUpdateCollection.Add($Update) | Out-Null
+        } #End If $DownloadResult.ResultCode -eq 2 -and $($update.EulaAccepted) -eq $true)
+      }
+
+      if ($DownloadedUpdateCollection.Count -eq 0)
+      {
+        Throw "No updates were downloaded."
+      }
+      else
+      {
+        Write-Output "Updates to install:"
+        foreach ($update in $DownloadedUpdateCollection)
+        {
+          Write-Output "$($update.Title)"
+        }
+      }
+      #-==Install Update==-
+      Write-Output "Now installing downloaded updates..."
+      $NeedsReboot=$false
+      $UpdateInstallationReboot = New-Object -ComObject "Microsoft.Update.UpdateColl"
+      foreach ($update in $DownloadedUpdateCollection)
+      {
+        $objCollectionTmp = New-Object -ComObject "Microsoft.Update.UpdateColl"
+        $objCollectionTmp.Add($Update) | Out-Null
+        $objInstaller = $objSession.CreateUpdateInstaller()
+        $objInstaller.Updates = $objCollectionTmp
+        try
+        {
+          Write-Output "Trying to install update $($update.Title).."
+          $InstallResult = $objInstaller.Install()
+        } #End Try
+        catch
+        {
+          throw "Error installing update $($update.Title)."
+        } #End Catch
+
+        if ($InstallResult.ResultCode -eq 2)
+        {
+          Write-Output "Update $($update.Title) installation successfully completed"
+          if ($($Update.RebootRequired) -eq $true)
+          {
+            Write-Output "$($update.Title) requires reboot to be applied."
+            $UpdateInstallationReboot.Add($update)
+          }
+        }
+        # if any update from collection require reboot, then mark sign NeedsReboot as true
+        if (($NeedsReboot -eq $false) -and ($($Update.RebootRequired) -eq $true))
+        {
+          $NeedsReboot = $Update.RebootRequired
+        }
+      }
+
+      if ($UpdateInstallationReboot -ne 0)
+      {
+        Write-Output "Updates that require reboot to take effect:"
+        foreach ($update in $UpdateInstallationReboot)
+        {
+          Write-Output "$($update.Title)"
+        }
+      }
+
+      if($NeedsReboot) #checks if any update requires reboot after installation
+      {
+        $AutoRebootAllowed = $#{node['autopatch_ii']['auto_reboot_enabled']}
+        if($AutoRebootAllowed)
+        {
+          Write-Output "Reboot is required, and Auto reboot is allowed on this machine. Rebooting NOW!"
+          Restart-Computer -Force
+        } #End If $AutoReboot
+        else
+        {
+          Write-Output "Manual reboot required! AutoReboot is NOT allowed on this machine."
+          return "Reboot is required, but not allowed by settings. Please do the reboot manually."
+        } #End Else $AutoReboot If $IgnoreReboot
+      } #End If $NeedsReboot
+      else
+      {
+        Write-Output "Reboot is not required."
       }
     }
-
-    WSUSUpdate
-    If ($Result.rebootRequired) { Restart-Computer }
+    catch
+    {
+      Write-Output $Error[0]
+      Write-Output ($Error[0].ErrorRecord.Exception | gm)
+      Exit 1
+    }
     EOH
-    action :nothing
     ignore_failure true
+    live_stream true
     notifies :request_reboot, 'reboot[firstrun_patches]', :delayed if node['autopatch_ii']['auto_reboot_enabled']
+    notifies :create_if_missing, "file[#{Chef::Config[:file_cache_path]}/autopatch.txt]", :immediately
+    not_if { ::File.exist?("#{Chef::Config[:file_cache_path]}/autopatch.txt") }
   end
 else
   raise 'OS unsupported for firstrun_patches recipe'
@@ -75,8 +205,7 @@ end
 
 file "#{Chef::Config[:file_cache_path]}/autopatch.txt" do
   content 'First run of patches will only run if this file does not exist.'
-  action :create_if_missing
-  notifies :run, platform_family?('windows') ? 'powershell_script[win-update]' : 'execute[linux-upgrade-once]', :immediately
+  action :nothing
 end
 
 reboot 'firstrun_patches' do
