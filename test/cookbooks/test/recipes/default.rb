28d63265a28aa5a7c1ebb610216717f46af4fafe
Corey Hemminger <hemminger@hotmail.com>
2022-06-27 11:25:16 -0500

    fix bug introduced from previous cookstyle fix
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

--- test/cookbooks/test/recipes/default.rb
+++ test/cookbooks/test/recipes/default.rb
@@ -2,6 +2,7 @@
 # attribute override
 node.default['autopatch_ii']['auto_reboot_enabled'] = false
 node.default['autopatch_ii']['email_notification_mode'] = 'none'
+node.default['autopatch_ii']['updates_to_skip'] = %w(openssh-server openssh-client)
 
 # runlist
 include_recipe 'autopatch_ii'
